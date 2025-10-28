d9f6518fb1c882ee0cd19845f7eba7fff7737797
sealboi8492 <jestefokiem@gmail.com>
2025-10-28 15:09:08 +0100

    Delete currencyCalc directory

--- currencyCalc/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.currencycalc {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens org.example.currencycalc to javafx.fxml;
-    exports org.example.currencycalc;
-}
